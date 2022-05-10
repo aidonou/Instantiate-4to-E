a41a113f8b2411deaa5455fe7ad1a3d16967b6db
aidonou <101400213+aidonou@users.noreply.github.com>
2022-05-10 13:50:04 -0300

    sacar info de usuario con UI

---
+++ Assets/DataImputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataImputManager : MonoBehaviour
+{
+    public Text txtUsername;
+
+public void ShowGreetings()
+    {
+    Debug.Log("Hola " + txtUsername.text);
+    }
+}
